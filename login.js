ea5bf8b01d2bc255dfcacbd59249bee5810913de
sofylopdev <sofylopdev@gmail.com>
2021-03-19 17:33:10 +0000

    finished mdc-101

---
+++ login.js
@@ -0,0 +1,10 @@
+import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
+
+//mdc.textField.attachTo(document.querySelector('.username'));
+
+new MDCRipple(document.querySelector('.cancel'));
+new MDCRipple(document.querySelector('.next'));
